15b0c99ca81262bbd8285cdba7cb47a91f563c69
Finanda Wafiq <finnandawafiq@gmail.com>
2022-11-15 19:59:17 +0700

    Membuat file index

---
+++ src/main/java/ws/c/idcard_pwsc/IdcardPwscApplication.java
@@ -0,0 +1,13 @@
+package ws.c.idcard_pwsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdcardPwscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdcardPwscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws/c/idcard_pwsc/IdcardPwscApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.c.idcard_pwsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdcardPwscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
